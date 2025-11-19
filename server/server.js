b99ac91b58ae97128145d049fb2fd7cbad659f9d
rustam <absattorovrusta2004@gmail.com>
2025-11-19 22:55:05 +0300

    postgres sozlandi

--- server/server.js
+++ server/server.js
@@ -1,7 +1,4 @@
 // Faqat local development uchun dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 
 const express = require('express')
 const cors = require('cors')
